e106b62f6bd5ab51e2d5f034000cc7f33c509f29
Peilun Huang <phuang61@my.bcit.ca>
2023-04-07 10:14:26 -0700

    save note function

--- script/script.js
+++ script/script.js
@@ -23,9 +23,53 @@ themeToggle.addEventListener('click', () => {
 const cancelB = document.querySelector('.cancel');
 const saveB = document.querySelector('.save');
 const text = document.querySelector('.textarea');
+const newNoteB = document.querySelector('.new_note');
 
 cancelB.addEventListener('click', () =>{
     text.classList.add('hid');
     saveB.classList.add('hid');
     cancelB.classList.add('hid');
+    text.classList.remove('show');
+    saveB.classList.remove('show');
+    cancelB.classList.remove('show');
+});
+
+newNoteB.addEventListener('click', () => {
+    text.classList.add('show');
+    saveB.classList.add('show');
+    cancelB.classList.add('show');
+  
+    if (text.value) {
+      text.value = '';
+    }
+  });
+
+const notes = document.querySelector('.notes');
+const notesArray = [];
+  
+  saveB.addEventListener('click', () => {
+    const title = prompt('Enter the title for your note:');
+    if (title) {
+      const note = {
+        title,
+        body: text.value,
+      };
+      notesArray.push(note);
+      const li = document.createElement('li');
+      li.textContent = note.title;
+      notes.appendChild(li);
+    }
+  });
+
+const sidebar = document.querySelector('.menu ul');
+
+sidebar.addEventListener('click', (event) => {
+  const clickedItem = event.target;
+  const clickedTitle = clickedItem.textContent;
+
+  const clickedNote = notesArray.find(note => note.title === clickedTitle);
+
+  if (clickedNote) {
+    text.value = clickedNote.body;
+  }
 });
